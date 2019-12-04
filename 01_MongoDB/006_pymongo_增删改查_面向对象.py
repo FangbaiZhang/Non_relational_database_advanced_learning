34e8fa3ec7f15cad82f4cfb37896f4b35394b4c2
Felix Zhang <18200116656@qq.com>
2019-12-04 16:55:16 +0800

    Daily Commit

--- 01_MongoDB/006_pymongo_增删改查_面向对象.py
+++ 01_MongoDB/006_pymongo_增删改查_面向对象.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
+# 注意，先要启动本地的MongoDB服务器
 
 from pymongo import MongoClient
 
-class TestMongo:
+class TestMongo():
+
+    # 初始化时自动创建连接
     def __init__(self):
-        # 初始化时自动创建连接
-        client = MongoClient()
-        # 使用方括号选择数据库及其集合
+
+        # 创建MongoDB客户端
+        client = MongoClient(host='127.0.0.1', port=27017)
+        # 使用方括号选择test数据库里面的t251集合
         self.collection = client["test"]["t251"]
         # 也可以使用.的方式
         # self.collection = client.test.t251
 
-
+# 实例化
 test_mongo = TestMongo()
